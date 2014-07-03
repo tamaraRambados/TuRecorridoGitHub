aa110cb87978ae70a04a52719a715a8aaf0e07fd
java <java@java-System-Product-Name>
2014-07-03 10:50:19 -0300

    otra version

--- TuRecorrido/src/com/example/turecorrido/MainActivity.java
+++ TuRecorrido/src/com/example/turecorrido/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.turecorrido;
+import com.example.turecorrido.SegundoActivity;
 
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
 public class MainActivity extends Activity {
@@ -16,14 +20,41 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.primero);//primero es el nombre de la variable del 1er layout
+		
+		// desde aca empiezo
+		
+		Button botonText = (Button) findViewById(R.id.textEliminarCuenta);
+		botonText.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// importo solo cuando me da error en new OnClickListener()
+				Intent intent= new Intent(MainActivity.this,SegundoActivity.class);
+				int PASARPANTALLA = 0;
+				Intent i = null;
+				startActivityForResult(i,PASARPANTALLA);
+			
+			
+			}
+		});
+		
+		
+		
+		
+		
+		
+		
+		/////////////////////////////////////
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
-
+	//////////////////////////////////////hasta no hice yo, ya estaba
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -55,7 +86,7 @@ public class MainActivity extends Activity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
+			View rootView = inflater.inflate(R.layout.Primero, container,
 					false);
 			return rootView;
 		}
